4aeefc5afa30d99a1e2781cd0e84541fec18a07b
sean.sinyi.lai <sean.sinyi.lai@gmail.com>
2021-10-19 15:47:07 +0800

    最後完整版

--- ModbusConnection/Tools.cs
+++ ModbusConnection/Tools.cs
@@ -8,11 +8,11 @@ namespace ModbusConnection
 {
     public static class Tools
     {/// <summary>
-    /// 將數值拆成高低位元
-    /// </summary>
-    /// <param name="val"></param>
-    /// <returns></returns>
-        public static byte[] SplitShortToHighAndLowByte(this ushort val) 
+     /// 將數值拆成高低位元
+     /// </summary>
+     /// <param name="val"></param>
+     /// <returns></returns>
+        public static byte[] SplitShortToHighAndLowByte(this ushort val)
         {
             byte[] tmp = new byte[2];
             string binartsaa = Convert.ToString(val, 2).PadLeft(16, '0');
@@ -22,10 +22,10 @@ namespace ModbusConnection
             tmp[1] = Convert.ToByte(Convert.ToInt32(lowbit, 2));
             return tmp;
         }
-        public static string ReverseString(this string val) 
+        public static string ReverseString(this string val)
         {
             string tmp = string.Empty;
-            for (int i = val.Length-1; i >= 0 ; i--)
+            for (int i = val.Length - 1; i >= 0; i--)
             {
                 tmp += val[i];
             }
