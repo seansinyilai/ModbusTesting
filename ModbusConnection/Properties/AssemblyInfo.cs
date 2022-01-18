8f0355b542ebe74286596d07b6412835662b47bc
sean.sinyi.lai <sean.sinyi.lai@gmail.com>
2022-01-18 19:11:52 +0800

    更新至1.4.0

--- ModbusConnection/Properties/AssemblyInfo.cs
+++ ModbusConnection/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // 變更這些屬性的值即可修改組件的相關
 // 資訊。
 [assembly: AssemblyTitle("ModbusConnection")]
-[assembly: AssemblyDescription("Modbus基礎底層")]
+[assembly: AssemblyDescription("Modbus基礎底層 更新多線圈寫入數量")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("GP Group_SinyiLai")]
 [assembly: AssemblyProduct("ModbusConnection")]
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // 您可以指定所有的值，也可以使用 '*' 將組建和修訂編號
 // 設為預設，如下所示:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.3.1")]
-[assembly: AssemblyFileVersion("1.0.3.1")]
+[assembly: AssemblyVersion("1.0.4.0")]
+[assembly: AssemblyFileVersion("1.0.4.0")]
